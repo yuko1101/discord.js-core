09a22d4844b0e0a9589c3a2858657013c08d1cfa
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-03-06 11:48:28 +0900

    update example

--- example/index.js
+++ example/index.js
@@ -4,25 +4,40 @@ import { Client, Intents } from "discord.js";
 import dotenv from "dotenv";
 dotenv.config();
 
+const command = new Command({
+    name: "ping",
+    description: "pong",
+    args: ["test"],
+    options: [
+        {
+            name: "test",
+            type: "STRING",
+            description: "test",
+            required: true
+        }
+    ],
+    type: "MESSAGE_COMMAND",
+    run: async (ic, args, core) => {
+        console.log(args);
+        return {
+            content: `pong ${args["test"]}`,
+        };
+    },
+    runAfter: async (ic, sent, args, core) => {
+        sent.edit(`pong ${args["test"]} again`);
+    }
+});
+
 const core = new Core(
     new Client({
-        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+        allowedMentions: { repliedUser: false }
     }),
     { token: process.env.TOKEN, prefix: "pt!", debug: true, guildId: "736829048373903377" }
 );
 
 core.login();
 
+core.addCommand(command);
 
-const command = new Command({
-    name: "ping",
-    description: "Pong!",
-    args: [],
-    options: [],
-    aliases: [],
-    run: async (msg, args, core) => {
-        return {
-            content: "Pong!"
-        };
-    }
-});
+core.client.on("ready", () => { core.applySlashCommands(); });
---
+++ example/index.ts
@@ -0,0 +1,13 @@
+import { Core } from "discord-core";
+import { Client, Intents } from "discord.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const core: Core = new Core(
+    new Client({
+        intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+    }),
+    { debug: true, prefix: "pt!", token: process.env.TOKEN, guildId: null }
+);
+
+core.login();
