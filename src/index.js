19595b42a54fff28d45502d46dfa01a30751da04
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-03-27 11:10:32 +0900

    add Pages and Actions

--- src/index.js
+++ src/index.js
@@ -4,14 +4,20 @@ const Core = require("./core/Core");
 const Command = require("./command/Command");
 // const Reaction = require("./Reaction");
 const ConfigFile = require("./utils/ConfigFile");
-// const Pages = require("./Pages");
+const Pages = require("./utils/Pages");
 const CustomEmoji = require("./utils/CustomEmoji");
+const EmojiAction = require("./action/EmojiAction");
+const ButtonAction = require("./action/ButtonAction");
+const Action = require("./action/Action");
 
 module.exports = {
     Core: Core,
     Command: Command,
     // Reaction,
     ConfigFile: ConfigFile,
-    // Pages,
-    CustomEmoji: CustomEmoji
+    Pages: Pages,
+    CustomEmoji: CustomEmoji,
+    EmojiAction: EmojiAction,
+    ButtonAction: ButtonAction,
+    Action: Action
 }
