5ecafd0aac969b45fb73ee6dd618356181cf2cbb
yuko1101 <68993883+yuko1101@users.noreply.github.com>
2022-03-30 18:33:11 +0900

    add timeout and deletion

--- src/action/EmojiAction.js
+++ src/action/EmojiAction.js
@@ -1,5 +1,6 @@
 const { MessageReaction, Snowflake, User, Message } = require("discord.js");
 const Core = require("../core/Core");
+const { bindOptions } = require("../utils/utils");
 const Action = require("./Action");
 
 module.exports = class EmojiAction extends Action {
@@ -24,21 +25,52 @@ module.exports = class EmojiAction extends Action {
 
         /** @readonly @type {Snowflake[]} */
         this.appliedMessages = [];
+
+        /** @readonly @type {boolean} */
+        this.deleted = false;
     }
 
     /**
      * @param {Message} message
+     * @param {object} options
+     * @param {number} options.timeout
+     * @param {boolean} options.autoReact
      */
-    async apply(message) {
+    async apply(message, options) {
+        options = bindOptions({ timeout: -1, autoReact: true }, options)
+
+        if (this.deleted) throw new Error("This emoji action has been deleted.");
+
         // if this action hasn't been registered to the core, register it
         if (!this.core.emojiActions.some(action => action.id === this.id)) {
             this.core.emojiActions.push(this);
         }
 
-        // if the message doesn't have the reaction, add it
-        if (!message.reactions.cache.some(reaction => reaction.emoji.toString() === this.label)) {
-            await message.react(this.label);
+
+        if (options.autoReact) {
+            // if the message doesn't have the reaction, add it
+            if (!message.reactions.resolve(this.label)) {
+                await message.react(this.label);
+            }
         }
         this.appliedMessages.push(message.id);
+
+        if (options.timeout !== -1) {
+            setTimeout(() => {
+                this.appliedMessages.splice(this.appliedMessages.indexOf(message.id), 1);
+                if (message.reactions.resolve(this.label)?.users?.resolve(this.core.client.user.id)) {
+                    message.reactions.resolve(this.label).users.remove(this.core.client.user);
+                }
+            }, options.timeout);
+        }
+
+    }
+
+    delete() {
+        const index = this.core.emojiActions.findIndex(action => action.id === this.id);
+        if (index !== -1) {
+            this.core.emojiActions.splice(index, 1);
+        }
+        this.deleted = true;
     }
 }
